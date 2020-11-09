200b93a55376f2627617a7c3aa65faa913e4b86c
Edward Poon <edwardpoon1130@gmail.com>
2020-11-09 07:32:04 -0500

    Initial Commit

---
+++ Mukbang/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Mukbang
+//
+//  Created by Edward Poon on 11/9/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
